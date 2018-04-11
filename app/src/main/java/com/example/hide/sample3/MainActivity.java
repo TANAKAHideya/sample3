08945c32100ecfa0ef8316bcff5abf7cd8f9ecef
TANAKA Hideya <hideya.tanaka.dev@gmail.com>
2018-04-11 20:18:07 +0900

    update
    update

--- app/src/main/java/com/example/hide/sample3/MainActivity.java
+++ app/src/main/java/com/example/hide/sample3/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.hide.sample3;
 
 import android.app.ActionBar;
+import android.app.ActivityOptions;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "sample3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        int displayid= ActivityOptions.makeBasic().getLaunchDisplayId();
+        Log.i(TAG, "sample3 displayid =" + displayid);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
